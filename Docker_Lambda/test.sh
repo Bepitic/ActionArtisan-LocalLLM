dbe078152f7c9e6f02324fee33c0efa4ba71018c
Bepitic <Bepitic@gmail.com>
2023-10-03 07:26:28 +0200

    started creation Docker-image lambda

---
+++ Docker_Lambda/test.sh
@@ -0,0 +1,9 @@
+docker run -p 9000:8080 docker-image:test
+
+# curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+
+curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"payload":"hello world!"}'
+
+docker ps
+
+docker kill 3766c4ab331c
