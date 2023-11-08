454720616515df64a0638b4ce7816e6d938deb3a
Bepitic <Bepitic@gmail.com>
2023-11-08 11:09:40 +0100

    Upload shell to test the lambda

--- Docker_Lambda/test.sh
+++ Docker_Lambda/test.sh
@@ -1,9 +1,9 @@
-docker run --name lambdatest -p 9000:8080 docker-image:test 
+docker run --name lambdatest -p 9000:8080 ActionArtisan-image:test 
 
 # curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
 
-curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"payload":"hello world!"}'
+# curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"payload":"hello world!"}'
 
-docker ps
+# docker ps
 
-docker kill lambdatest
+# docker kill lambdatest
